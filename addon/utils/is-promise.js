08305b42aa41b3d57e64983a1ec60ec1a187fac2
Lauren Tan <lauren.elizabeth.tan@gmail.com>
2016-03-05 14:15:48 -0500

    Make `pipe` work with PromiseProxies
    - Relaxed the `instanceof` check to duck type if the object implements
      Promise-like methods to work with Objects that include the
      PromiseProxy mixin

--- addon/utils/is-promise.js
+++ addon/utils/is-promise.js
@@ -1,7 +1,13 @@
 import Ember from 'ember';
 
-const { RSVP: { Promise }, isPresent } = Ember;
+const { typeOf } = Ember;
+
+function isPromiseLike(obj = {}) {
+  return typeOf(obj.then) === 'function' &&
+    typeOf(obj.catch) === 'function' &&
+    typeOf(obj.finally) === 'function';
+}
 
 export default function isPromise(obj) {
-  return isPresent(obj) && obj instanceof Promise;
+  return typeOf(obj) === 'object' && isPromiseLike(obj);
 }
