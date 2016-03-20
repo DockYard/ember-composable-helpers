6e9e5d378df776a429e9016348d0e3c842faa6c2
Marten Schilstra <mail@martenschilstra.nl>
2016-03-20 10:51:50 +0100

    DRY up append, intersect and union

---
+++ addon/-private/create-multi-array-helper.js
@@ -0,0 +1,65 @@
+import Ember from 'ember';
+import {
+  A as emberArray,
+  isEmberArray as isArray
+} from 'ember-array/utils';
+import Helper from 'ember-helper';
+import { guidFor } from 'ember-metal/utils';
+import computed from 'ember-computed';
+import observer from 'ember-metal/observer';
+import get from 'ember-metal/get';
+import set from 'ember-metal/set';
+import { isEmpty } from 'ember-utils';
+
+const { defineProperty } = Ember;
+const idForArray = (array) => `__array-${guidFor(array)}`;
+
+export default function(multiArrayComputed) {
+  return Helper.extend({
+    compute([...arrays]) {
+      set(this, 'arrays', arrays.map((obj) => {
+        if (isArray(obj)) {
+          return emberArray(obj)
+        }
+
+        return obj;
+      }));
+
+      return get(this, 'content');
+    },
+
+    valuesDidChange: observer('arrays.[]', function() {
+      this._recomputeArrayKeys();
+
+      let arrays = get(this, 'arrays');
+      let arrayKeys = get(this, 'arrayKeys');
+
+      if (isEmpty(arrays)) {
+        defineProperty(this, 'content', []);
+        return;
+      }
+
+      defineProperty(this, 'content', multiArrayComputed(...arrayKeys));
+    }),
+
+    contentDidChange: observer('content.[]', function() {
+      this.recompute();
+    }),
+
+    _recomputeArrayKeys() {
+      let arrays = get(this, 'arrays');
+
+      let oldArrayKeys = get(this, 'arrayKeys') || [];
+      let newArrayKeys = arrays.map(idForArray);
+
+      let keysToRemove = oldArrayKeys.filter((key) => {
+        return newArrayKeys.indexOf(key) === -1;
+      });
+
+      keysToRemove.forEach((key) => set(this, key, null));
+      arrays.forEach((array) => set(this, idForArray(array), array));
+
+      set(this, 'arrayKeys', newArrayKeys);
+    }
+  });
+}
--- addon/helpers/append.js
+++ addon/helpers/append.js
@@ -1,22 +1,15 @@
 import Ember from 'ember';
-import { isEmberArray as isArray } from 'ember-array/utils';
 import computed from 'ember-computed';
-import Helper from 'ember-helper';
 import get from 'ember-metal/get';
-import observer from 'ember-metal/observer';
-import set from 'ember-metal/set';
-import { guidFor } from 'ember-metal/utils';
-import { isEmpty } from 'ember-utils';
-
-const { defineProperty } = Ember;
-
-const idForArray = (array) => `__array-${guidFor(array)}`;
+import { isEmberArray as isArray } from 'ember-array/utils';
+import createMultiArrayHelper from '../-private/create-multi-array-helper';
 
 export function append(...dependentKeys) {
   dependentKeys = dependentKeys || [];
   let arrayKeys = dependentKeys.map((dependentKey) => {
     return `${dependentKey}.[]`;
   });
+
   return computed(...arrayKeys, function() {
     let array = dependentKeys.map((dependentKey) => {
       let value = get(this, dependentKey);
@@ -27,37 +20,4 @@ export function append(...dependentKeys) {
   });
 }
 
-export default Helper.extend({
-  compute([...arrays]) {
-    set(this, 'arrays', arrays);
-
-    return get(this, 'content');
-  },
-
-  valuesDidChange: observer('arrays.[]', function() {
-    let arrays = get(this, 'arrays');
-
-    let oldArrayKeys = get(this, 'arrayKeys') || [];
-    let newArrayKeys = arrays.map(idForArray);
-
-    let keysToRemove = oldArrayKeys.filter((key) => {
-      return newArrayKeys.indexOf(key) === -1;
-    });
-
-    keysToRemove.forEach((key) => set(this, key, null));
-    arrays.forEach((array) => set(this, idForArray(array), array));
-
-    set(this, 'arrayKeys', newArrayKeys);
-
-    if (isEmpty(arrays)) {
-      defineProperty(this, 'content', []);
-      return;
-    }
-
-    defineProperty(this, 'content', append(...newArrayKeys));
-  }),
-
-  contentDidChange: observer('content.[]', function() {
-    this.recompute();
-  })
-});
+export default createMultiArrayHelper(append);
