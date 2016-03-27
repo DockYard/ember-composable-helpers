84ea4f6022fb0b157689d5518780e1e6a99915cb
Lauren Tan <lauren.elizabeth.tan@gmail.com>
2016-03-27 11:33:06 -0400

    Clean up imports in `invoke` helper

--- addon/helpers/invoke.js
+++ addon/helpers/invoke.js
@@ -1,25 +1,24 @@
-import Ember from 'ember';
+import { isEmberArray } from 'ember-array/utils';
+import { helper } from 'ember-helper';
+import { tryInvoke } from 'ember-utils';
+import RSVP from 'rsvp';
 
-const {
-  RSVP: { all },
-  Helper: { helper },
-  isArray,
-  tryInvoke
-} = Ember;
+const { all } = RSVP;
 
 export function invoke([methodName, ...args]) {
   let obj = args.pop();
-  if (isArray(obj)) {
+
+  if (isEmberArray(obj)) {
     return function() {
-      return all(
-        obj.map((item) => tryInvoke(item, methodName, args))
-      );
-    };
-  } else {
-    return function() {
-      return tryInvoke(obj, methodName, args);
+      let promises = obj.map((item) => tryInvoke(item, methodName, args));
+
+      return all(promises);
     };
   }
+
+  return function() {
+    return tryInvoke(obj, methodName, args);
+  };
 }
 
 export default helper(invoke);
