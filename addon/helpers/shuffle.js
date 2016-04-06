e8c3a41bbea9e3981f8f95fe67c21e6e4836f0e6
Robbie Speed <robert.y.speed@gmail.com>
2016-04-05 21:58:43 -0700

    whitespace removal

--- addon/helpers/shuffle.js
+++ addon/helpers/shuffle.js
@@ -12,10 +12,10 @@ export function shuffle(array, randomizer) {
   let count = array.length;
   let rand, temp;
   randomizer = (typeOf(randomizer) === 'function' && randomizer) || Math.random;
-  
+
   while (count > 1) {
     rand = Math.floor(randomizer() * (count--));
-    
+
     temp = array[count];
     array[count] = array[rand];
     array[rand] = temp;
@@ -28,11 +28,11 @@ export default Helper.extend({
     if (!isArray(array)) {
       return emberArray([array]);
     }
-    
+
     set(this, 'array', array);
     return shuffle(array, random);
   },
-  
+
   arrayContentDidChange: observer('array.[]', function() {
     this.recompute();
   })
