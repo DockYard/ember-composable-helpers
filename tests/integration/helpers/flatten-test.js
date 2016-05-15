c96e2970b1885a18a918aec05a56d835d88e46b9
Asher Cohen <asherlc@asherlc.com>
2016-05-15 15:14:24 -0700

    Fix test name

--- tests/integration/helpers/flatten-test.js
+++ tests/integration/helpers/flatten-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('append', 'Integration | Helper | {{append}}', {
+moduleForComponent('flatten', 'Integration | Helper | {{flatten}}', {
   integration: true
 });
 
