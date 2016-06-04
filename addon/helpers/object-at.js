60af506e663f89578318d1d6567963e678fd86e8
Garrett Murphey <gmurphey@gmurphey.com>
2016-06-04 13:38:42 -0400

    Implementing object-at helper.

---
+++ addon/helpers/object-at.js
@@ -0,0 +1,36 @@
+import Helper from 'ember-helper';
+import { A as emberArray, isEmberArray as isArray } from 'ember-array/utils';
+import computed from 'ember-computed';
+import observer from 'ember-metal/observer';
+import get from 'ember-metal/get';
+import set from 'ember-metal/set';
+
+export function objectAt(index, array) {
+  if (!isArray(array)) {
+    return undefined;
+  }
+
+  index = parseInt(index, 10);
+
+  return emberArray(array).objectAt(index);
+}
+
+export default Helper.extend({
+  content: computed('index', 'array.[]', function() {
+    let index = get(this, 'index');
+    let array = get(this, 'array');
+
+    return objectAt(index, array);
+  }),
+
+  compute([index, array]) {
+    set(this, 'index', index);
+    set(this, 'array', array);
+
+    return get(this, 'content');
+  },
+
+  contentDidChange: observer('content', function() {
+    this.recompute();
+  })
+});
