bda6e1af4ee8ceaf94a844eb95589a148b31a021
Taras Mankovski <tarasm@gmail.com>
2016-07-18 22:40:57 -0400

    Added filter helper

---
+++ tests/unit/helpers/filter-test.js
@@ -0,0 +1,10 @@
+import { filter } from 'dummy/helpers/filter';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | filter');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = filter([42]);
+  assert.ok(result);
+});
