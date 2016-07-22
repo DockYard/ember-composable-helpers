fcccaf676e8da97cc94c408c9c6b071e19845513
Marten Schilstra <mail@martenschilstra.nl>
2016-07-22 15:53:32 +0200

    Remove PR # from randomization

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,10 +1,5 @@
 /*jshint node:true*/
 var command = [ 'ember', 'exam', '--split', '3', '--random' ];
-var pr = process.env.TRAVIS_PULL_REQUEST;
-
-if (pr) {
-  command.push(pr);
-}
 
 module.exports = {
   command: command.join(' ')
