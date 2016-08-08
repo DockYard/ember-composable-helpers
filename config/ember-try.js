71950c21d544fc527567660160940fc687d090d9
Marten Schilstra <mail@martenschilstra.nl>
2016-08-08 19:54:26 +0200

    Update Ember Try scenarios

--- config/ember-try.js
+++ config/ember-try.js
@@ -3,8 +3,66 @@ var command = [ 'ember', 'exam', '--split', '3', '--random' ];
 
 module.exports = {
   command: command.join(' '),
-  useVersionCompatibility: true,
   scenarios: [
+    {
+      name: 'ember-pre-2',
+      bower: {
+        dependencies: {
+          "ember": "~1.13.0"
+        }
+      }
+    },
+    {
+      name: 'ember-2',
+      bower: {
+        dependencies: {
+          "ember": "~2.0.0"
+        }
+      }
+    },
+    {
+      name: 'ember-lts',
+      bower: {
+        dependencies: {
+          "ember": "~2.4.0"
+        }
+      }
+    },
+    {
+      name: 'ember-latest',
+      bower: {
+        dependencies: {
+          "ember": "release"
+        },
+        resolutions: {
+          "ember": "release"
+        }
+      }
+    },
+    {
+      name: 'ember-beta',
+      allowedToFail: true,
+      bower: {
+        dependencies: {
+          "ember": "beta"
+        },
+        resolutions: {
+          "ember": "beta"
+        }
+      }
+    },
+    {
+      name: 'ember-canary',
+      allowedToFail: true,
+      bower: {
+        dependencies: {
+          "ember": "canary"
+        },
+        resolutions: {
+          "ember": "canary"
+        }
+      }
+    },
     {
       name: 'ember-alpha',
       allowedToFail: true,
