974d5c119adae971894b016487c0c8a5e6506727
Marten Schilstra <mail@martenschilstra.nl>
2016-08-25 11:02:08 +0200

    Fixes titleize properly handling null

--- addon/helpers/titleize.js
+++ addon/helpers/titleize.js
@@ -1,8 +1,6 @@
 import { helper } from 'ember-helper';
 import titleizeLib from 'ember-composable-helpers/utils/titleize';
+import createStringHelperFunction from '../-private/create-string-helper';
 
-export function titleize([str = '']) {
-  return titleizeLib(str);
-}
-
+export const titleize = createStringHelperFunction(titleizeLib);
 export default helper(titleize);
