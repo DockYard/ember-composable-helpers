988e7d11ffcec60172dfe12e3d8d9f4a1985dfb5
Garrett Murphey <gmurphey@gmurphey.com>
2017-01-28 05:23:04 -0500

    Updating next and previous helpers to handle ember-data relationships. (#241)
    Fixes #229.

---
+++ tests/dummy/app/models/person.js
@@ -0,0 +1,8 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { hasMany } from 'ember-data/relationships';
+
+export default Model.extend({
+  name: attr('string'),
+  pets: hasMany('pet')
+});
