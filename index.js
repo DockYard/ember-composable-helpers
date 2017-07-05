40abb43590a27b622679977041ae13902e80a1c0
Lauren Tan <poteto@users.noreply.github.com>
2017-07-06 02:25:51 +0800

    Fix filter helpers regexp (#275)

--- index.js
+++ index.js
@@ -28,7 +28,8 @@ module.exports = {
   treeForAddon: function() {
     // see: https://github.com/ember-cli/ember-cli/issues/4463
     var tree = this._super.treeForAddon.apply(this, arguments);
-    return this.filterHelpers(tree, new RegExp('^modules\/' + this.name + '\/helpers\/', 'i'));
+    var moduleRegexp = '(^modules\/)?' + this.name + '\/helpers\/';
+    return this.filterHelpers(tree, new RegExp(moduleRegexp, 'i'));
   },
 
   filterHelpers: function(tree, regex) {
