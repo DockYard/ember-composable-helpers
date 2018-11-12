23b405c3825cf6c1e48f886080abeba598cfd814
Balint Erdi <balint.erdi@gmail.com>
2018-11-12 16:01:52 +0100

    Remove ember-cli-shims dependency

--- blueprints/ember-composable-helpers/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  normalizeEntityName: function() {},
-
-  afterInstall: function(options) {
-    if (!('ember-cli-shims' in options.project.addonPackages)) {
-      return this.addBowerPackageToProject('ember-cli-shims', '~0.1.1');
-    }
-  }
-};
