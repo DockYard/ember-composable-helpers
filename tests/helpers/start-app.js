9e4dcaa7d40cf63872fefb42013473a0cb619954
Scott Newcomer <snewcomer24@gmail.com>
2021-12-02 21:17:08 -0600

    moar cleanup

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -1,16 +1,10 @@
-import {
-  merge as emberMerge,
-  assign as emberAssign
-} from '@ember/polyfills';
 import { run } from '@ember/runloop';
 import Application from '../../app';
 import config from '../../config/environment';
 
-const assign = emberAssign || emberMerge;
-
 export default function startApp(attrs) {
-  let attributes = assign({}, config.APP);
-  attributes = assign(attributes, attrs); // use defaults, but you can override;
+  let attributes = Object.assign({}, config.APP);
+  attributes = Object.assign(attributes, attrs); // use defaults, but you can override;
 
   return run(() => {
     let application = Application.create(attributes);
