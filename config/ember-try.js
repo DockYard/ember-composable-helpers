003c85fd06eb0bb8f1b2776771f3b1b11f650a87
Miguel Camba <miguel.camba@gmail.com>
2021-12-07 22:21:32 +0100

    Remove useless scenario in 2022

--- config/ember-try.js
+++ config/ember-try.js
@@ -64,19 +64,6 @@ module.exports = async function() {
           devDependencies: {}
         }
       },
-      {
-        name: 'ember-default-with-jquery',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'jquery-integration': true
-          })
-        },
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^0.5.1'
-          }
-        }
-      },
       embroiderSafe(),
       embroiderOptimized(),
     ]
